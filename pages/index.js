f4aabca80a5d730d910359f03d29f7bb3f8c9580
Steven McDonald <stevenpmcdonald@gmail.com>
2023-04-17 13:12:13 -0400

    Branding push
    Favicon and launching soon page.

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Culper Solutions</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Culper Solutions" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Website launching <code>soon.</code>
         </p>
       </main>
 
